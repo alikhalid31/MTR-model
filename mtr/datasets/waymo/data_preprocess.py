c1a36f7549da4ee7f19a2c030e7ea2b4a2152a6b
Ali khalid <alikhalid3110@gmail.com>
2025-07-01 18:05:16 -0400

    added ego predict filter in data preprocessing

--- mtr/datasets/waymo/data_preprocess.py
+++ mtr/datasets/waymo/data_preprocess.py
@@ -14,7 +14,109 @@ from tqdm import tqdm
 from waymo_open_dataset.protos import scenario_pb2
 from waymo_types import object_type, lane_type, road_line_type, road_edge_type, signal_state, polyline_type
 
-    
+# import pandas as pd
+# from shapely.geometry import LineString
+# from itertools import combinations
+
+# function to count total intersections between all the segments of polylines
+# def count_polyline_intersections(polylines):
+#     """
+#     Given a list of polylines (each is a list of (x, y) tuples),
+#     count how many times the line segments intersect each other.
+
+#     Args:
+#         polylines: List of polylines, where each polyline is a list of (x, y) tuples.
+
+#     Returns:
+#         Total number of pairwise segment intersections.
+#     """
+#     segments = []
+
+#     # Break each polyline into line segments
+#     for polyline in polylines:
+#         for i in range(len(polyline) - 1):
+#             segment = LineString([polyline[i], polyline[i + 1]])
+#             segments.append(segment)
+
+#     # Count all unique intersections
+#     intersection_count = 0
+#     for seg1, seg2 in combinations(segments, 2):
+#         if seg1.crosses(seg2) or seg1.intersects(seg2):
+#             if seg1.intersection(seg2).geom_type in ['Point', 'MultiPoint', 'LineString']:
+#                 intersection_count += 1
+
+#     return intersection_count
+
+# funtion to count intersections between polylines, but only once per pair of polylines
+def count_polyline_intersections_unique(polylines):
+    """
+    Given a list of polylines (each is a list of (x, y) tuples),
+    count how many times polylines intersect each other — only once per polyline pair.
+
+    Args:
+        polylines: List of polylines, where each polyline is a list of (x, y) tuples.
+
+    Returns:
+        Total number of unique polyline pair intersections.
+    """
+    # Convert each polyline into list of segments
+    all_segments = []
+    for polyline in polylines:
+        segments = []
+        for i in range(len(polyline) - 1):
+            segment = LineString([polyline[i], polyline[i + 1]])
+            segments.append(segment)
+        all_segments.append(segments)
+
+    intersected_pairs = set()
+
+    # Check all unique pairs of polylines
+    for i, j in combinations(range(len(polylines)), 2):
+        segments_i = all_segments[i]
+        segments_j = all_segments[j]
+
+        # Check if any segment from polyline i intersects any segment from polyline j
+        for seg1 in segments_i:
+            for seg2 in segments_j:
+                if seg1.crosses(seg2) or seg1.intersects(seg2):
+                    if seg1.intersection(seg2).geom_type in ['Point', 'MultiPoint', 'LineString']:
+                        intersected_pairs.add((i, j))
+                        break  # Stop checking this pair once intersection is found
+            else:
+                continue
+            break
+
+    return len(intersected_pairs)
+
+def populate_dataframe(info, track_infos, interactions_count):
+    rows=[]
+    scenario_id = info['scenario_id']
+    current_time_index = info['current_time_index']
+
+    for idx, track in enumerate(track_infos['trajs']):
+        object_id = track_infos['object_id'][idx]
+        object_type = track_infos['object_type'][idx]
+        # Split into past and future trajectories
+        past_traj = track[:current_time_index]
+        future_traj = track[current_time_index:]
+        curr_traj = track[current_time_index ]
+
+        # The last column (index 9) is the `valid` flag
+        valid_past = int(np.sum(past_traj[:, 9]))     # counts how many are valid in past
+        valid_future = int(np.sum(future_traj[:, 9])) # counts how many are valid in future
+        valid_current = int(np.sum(curr_traj[9])) # valid at current time index
+        
+        to_predict = False
+        if idx in info['tracks_to_predict']['track_index']:
+            to_predict = True
+
+        rows.append([
+            scenario_id, object_id, object_type, 
+            valid_past, valid_future, valid_current, to_predict, interactions_count
+        ])
+    return rows
+
+
 def decode_tracks_from_proto(tracks):
     track_infos = {
         'object_id': [],  # {0: unset, 1: vehicle, 2: pedestrian, 3: cyclist, 4: others}
@@ -170,11 +272,16 @@ def decode_dynamic_map_states_from_proto(dynamic_map_states):
 def process_waymo_data_with_scenario_proto(data_file, output_path=None):
     dataset = tf.data.TFRecordDataset(data_file, compression_type='')
     ret_infos = []
+    # df_rows=[]
     for cnt, data in enumerate(dataset):
         info = {}
         scenario = scenario_pb2.Scenario()
         # scenario.ParseFromString(bytearray(data.numpy()))
         scenario.ParseFromString(bytes(data.numpy()))
+
+        # if condition to filter out the scenarios with no tracks to predict
+        if len(scenario.tracks_to_predict) != 1:
+            continue
 
         info['scenario_id'] = scenario.scenario_id
         info['timestamps_seconds'] = list(scenario.timestamps_seconds)  # list of int of shape (91)
@@ -185,28 +292,29 @@ def process_waymo_data_with_scenario_proto(data_file, output_path=None):
         track_infos = decode_tracks_from_proto(scenario.tracks)
 
         # filtering tracks to predict based on speed
-        track_index= [cur_pred.track_index for cur_pred in scenario.tracks_to_predict]
-        track_index_filter =[]
-        for index in track_index:
-            single_track = track_infos['trajs'][index] 
-            # 10 = timestamp, 7 = velocity_x, 8 = velocity_y
-            speed = np.sqrt(single_track[10][7]**2 + single_track[10][8]**2)
-            if speed >= 21 and speed <= 1000:
-                track_index_filter.append(index)
-                # print(speed)
+        # track_index= [cur_pred.track_index for cur_pred in scenario.tracks_to_predict]
+
+        # track_index_filter =[]
+        # for index in track_index:
+        #     single_track = track_infos['trajs'][index] 
+        #     # 10 = timestamp, 7 = velocity_x, 8 = velocity_y
+        #     speed = np.sqrt(single_track[10][7]**2 + single_track[10][8]**2)
+        #     if speed >= 21 and speed <= 1000:
+        #         track_index_filter.append(index)
+        #         # print(speed)
 
 
 
         # modified this code to filter out the tracks wrt to speed
-        info['tracks_to_predict'] = {
-            'track_index': [cur_pred.track_index for cur_pred in scenario.tracks_to_predict if cur_pred.track_index in track_index_filter],
-            'difficulty': [cur_pred.difficulty for cur_pred in scenario.tracks_to_predict if cur_pred.track_index in track_index_filter]
-        }
-
         # info['tracks_to_predict'] = {
-        #     'track_index': [cur_pred.track_index for cur_pred in scenario.tracks_to_predict],
-        #     'difficulty': [cur_pred.difficulty for cur_pred in scenario.tracks_to_predict]
-        # }  # for training: suggestion of objects to train on, for val/test: need to be predicted
+        #     'track_index': [cur_pred.track_index for cur_pred in scenario.tracks_to_predict if cur_pred.track_index in track_index_filter],
+        #     'difficulty': [cur_pred.difficulty for cur_pred in scenario.tracks_to_predict if cur_pred.track_index in track_index_filter]
+        # }
+
+        info['tracks_to_predict'] = {
+            'track_index': [cur_pred.track_index for cur_pred in scenario.tracks_to_predict],
+            'difficulty': [cur_pred.difficulty for cur_pred in scenario.tracks_to_predict]
+        }  # for training: suggestion of objects to train on, for val/test: need to be predicted
 
         # if conditoin to filter scenerio based on number of agents (tracks) in the scene 
         #if not (len(scenario.tracks) >=100 and len(scenario.tracks) < 1000):
@@ -219,6 +327,12 @@ def process_waymo_data_with_scenario_proto(data_file, output_path=None):
         map_infos = decode_map_features_from_proto(scenario.map_features)
         dynamic_map_infos = decode_dynamic_map_states_from_proto(scenario.dynamic_map_states)
 
+
+        # code to create a dataframe to find data stats
+        # ego_polylines = [track_infos['trajs'][i][:,:2] for i in info['tracks_to_predict']['track_index']]
+        # interactions_count = count_polyline_intersections_unique(ego_polylines)
+        # df_rows += populate_dataframe(info, track_infos,interactions_count)
+
         save_infos = {
             'track_infos': track_infos,
             'dynamic_map_infos': dynamic_map_infos,
@@ -230,8 +344,10 @@ def process_waymo_data_with_scenario_proto(data_file, output_path=None):
         with open(output_file, 'wb') as f:
             pickle.dump(save_infos, f)
 
+
         ret_infos.append(info)
-    return ret_infos
+    # return df_rows, ret_infos
+    return  ret_infos
 
 
 def get_infos_from_protos(data_path, output_path=None, num_workers=8):
@@ -246,12 +362,20 @@ def get_infos_from_protos(data_path, output_path=None, num_workers=8):
     src_files = glob.glob(os.path.join(data_path, '*.tfrecord*'))
     src_files.sort()
 
+
     # func(src_files[0])
+    # all_df_rows= []
+    # all_infos = []
     with multiprocessing.Pool(num_workers) as p:
         data_infos = list(tqdm(p.imap(func, src_files), total=len(src_files)))
+        # for df_rows, data_infos in tqdm(p.imap(func, src_files), total=len(src_files)):
+        #     all_df_rows.extend(df_rows)
+        #     all_infos.extend(data_infos)
 
     all_infos = [item for infos in data_infos for item in infos]
-    return all_infos
+    # return all_df_rows, all_infos
+
+    return  all_infos
 
 
 def create_infos_from_protos(raw_data_path, output_path, num_workers=1):
@@ -265,25 +389,33 @@ def create_infos_from_protos(raw_data_path, output_path, num_workers=1):
     #     pickle.dump(train_infos, f)
     # print('----------------Waymo info train file is saved to %s----------------' % train_filename)
 
-    val_infos = get_infos_from_protos(
-        data_path=os.path.join(raw_data_path, 'validation'),
-        output_path=os.path.join(output_path, 'processed_scenarios_validation_speed_btw_21_inf'),
-        num_workers=num_workers
-    )
-    val_filename = os.path.join(output_path, 'processed_scenarios_val_speed_btw_21_inf_infos.pkl')
-    with open(val_filename, 'wb') as f:
-        pickle.dump(val_infos, f)
-    print('----------------Waymo info val file is saved to %s----------------' % val_filename)
-    
-    # debub_infos = get_infos_from_protos(
-    #     data_path=os.path.join(raw_data_path, 'debuging'),
-    #     output_path=os.path.join(output_path, 'processed_scenarios_debuging_lt_50'),
+    # val_infos = get_infos_from_protos(
+    #     data_path=os.path.join(raw_data_path, 'validation'),
+    #     output_path=os.path.join(output_path, 'processed_scenarios_validation_speed_btw_21_inf'),
     #     num_workers=num_workers
     # )
-    # debug_filename = os.path.join(output_path, 'processed_scenarios_debuging_lt_50_infos.pkl')
-    # with open(debug_filename, 'wb') as f:
-    #     pickle.dump(debub_infos, f)
-    # print('----------------Waymo info debug file is saved to %s----------------' % debug_filename)
+    # val_filename = os.path.join(output_path, 'processed_scenarios_val_speed_btw_21_inf_infos.pkl')
+    # with open(val_filename, 'wb') as f:
+    #     pickle.dump(val_infos, f)
+    # print('----------------Waymo info val file is saved to %s----------------' % val_filename)
+    
+    # df_rows, 
+    debug_infos = get_infos_from_protos(
+        data_path=os.path.join(raw_data_path, 'validation'),
+        output_path=os.path.join(output_path, 'processed_scenarios_validation_ego_1'),
+        num_workers=num_workers
+    )
+    debug_filename = os.path.join(output_path, 'processed_scenarios_val_ego_1_infos.pkl')
+    # df = pd.DataFrame(df_rows, columns=[
+    # 'scenario', 'agnet_id', 'object_type',
+    # 'past_valid_stamps', 'future_valid_stamps',
+    # 'current_valid_timestamp', 'to_predict', 'interactions_count'
+    # ])
+    # df.to_csv(os.path.join(output_path, 'processed_scenarios_validation.csv'), index=False) 
+   
+    with open(debug_filename, 'wb') as f:
+        pickle.dump(debug_infos, f)
+    print('----------------Waymo info debug file is saved to %s----------------' % debug_filename)
 
 
 if __name__ == '__main__':
---
+++ mtr/datasets/waymo/data_stats.py
@@ -0,0 +1,195 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def get_valid_timestamps_ego(df,ego=True):
+    """
+    Get the count of each agent type in the DataFrame.
+    """
+    df_filtered = df[df['to_predict']==ego]
+
+    df_past_stamps= df['past_valid_stamps'].value_counts().sort_index()
+    df_future_stamps= df['future_valid_stamps'].value_counts().sort_index()
+    df_current_stamps= df['current_valid_stamps'].value_counts().sort_index()
+    return df_past_stamps, df_future_stamps, df_current_stamps
+
+def get_agent_and_ego_count(df,ego_col='ego', agents_col='agents'):
+    """
+    Get the count of each agent type in the DataFrame.
+    """
+    grouped_counts = df.groupby('scenario')['to_predict'].value_counts().unstack(fill_value=0)
+    grouped_counts = grouped_counts.rename(columns={True: ego_col, False: agents_col})
+    # print(grouped_counts)
+    return grouped_counts
+
+def get_frequency_of_unique_values(df, column_name):
+    """
+    Get the frequency of unique values in a specified column.
+    """
+    return df[column_name].value_counts().sort_index()
+
+
+def plot_hitogram(df, xlabel='', ylabel='', title='', output_file='histogram.png'):
+    """
+    Plot a histogram (bar chart) from a Series or DataFrame.
+    """
+    plt.figure()  # Create a new figure to avoid overlaps
+    df.plot(kind='bar', figsize=(10, 5))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.grid(True)
+    plt.tight_layout()  # Optional: auto-adjust layout
+
+    # Save the plot
+    plt.savefig(output_file, dpi=300, bbox_inches='tight')
+    plt.close()  #
+
+
+def get_ego_histogram(df, column_name, xlabel='', ylabel='', title='', output_file='histogram.png', prefix=''):
+
+    # Get frequency of unique values in 'scenario' column
+    hist_ego = get_frequency_of_unique_values(df, column_name)
+
+    plot_hitogram(hist_ego,
+    xlabel=xlabel,
+    ylabel=ylabel,
+    title=title,
+    output_file=prefix+output_file)
+
+def get_agent_histogram(df, column_name, xlabel='', ylabel='', title='', output_file='histogram.png', prefix='', filter=0):
+
+    if filter > 0:
+        # Filter the DataFrame to only include rows where the 'agents' column is greater than the filter value
+        df = df[df['ego'] == filter]
+    # Bin directly on the agents column in df_count
+    bins = [0, 10, 20, 30, 50, 100, 200, 1000]  # 8 edges
+    labels = ['0–10', '11–20', '21–30', '31–50', '51–100', '101–200', '200+']  # 7 labels
+    df['agents_binned'] = pd.cut(df['agents'], bins=bins, labels=labels, right=False)
+
+    hist_agents_binned = get_frequency_of_unique_values(df,'agents_binned')
+    plot_hitogram(hist_agents_binned, 
+    xlabel=xlabel,
+    ylabel=ylabel,
+    title=title,
+    output_file=prefix+output_file)
+
+
+def get_interactions_histogram(df, column_name, xlabel='', ylabel='', title='', output_file='histogram.png', prefix='', filter=0):
+
+    if filter > 0:
+        # Filter the DataFrame to only include rows where the 'agents' column is greater than the filter value
+        df = df[df['to_predict_count'] == filter]
+
+    hist_agents_binned = get_frequency_of_unique_values(df,column_name)
+    plot_hitogram(hist_agents_binned, 
+    xlabel=xlabel,
+    ylabel=ylabel,
+    title=title,
+    output_file=prefix+output_file)
+
+def get_count_histograms(df):
+    """
+    Get histograms for the count of agents and ego vehicles in the DataFrame.
+    """
+    # Get agent and ego counts
+    df_count = get_agent_and_ego_count(df, ego_col='ego', agents_col='agents')
+
+    # save histograms
+    get_ego_histogram(df_count, 'ego',
+        xlabel='Number of Ego Tracks',
+        ylabel='Number of Scenarios',
+        title='Histogram of Ego Vehicle Count per Scenario',
+        output_file='ego_histogram.png', prefix='validation_dataset_')
+
+    get_agent_histogram(df_count, 'agents',
+        xlabel='Number of Agent Tracks',
+        ylabel='Number of Scenarios',
+        title='Histogram of Agent Count per Scenario',
+        output_file='agent_histogram.png', prefix='validation_dataset_')
+    
+    for i in range(1,9):
+        get_agent_histogram(df_count, 'agents',
+            xlabel='Number of Agent Tracks',
+            ylabel='Number of Scenarios',
+            title='Histogram of Agent Count per Scenario',
+            output_file='agent_histogram.png', prefix='validation_dataset_ego'+str(i)+'_', filter=i)
+    
+    
+def get_timestamps_histograms(df):
+    df_filtered_ego = df[df['to_predict']== True]
+    get_ego_histogram(df_filtered_ego, 'past_valid_stamps',
+    xlabel='Number of valid timestamps',
+    ylabel='Number of ego vehicles',
+    title='Histogram of past valid timestamps for ego vehicles',
+    output_file='ego_past_time_histogram.png', prefix='validation_dataset_')
+
+    get_ego_histogram(df_filtered_ego, 'future_valid_stamps',
+    xlabel='Number of valid timestamps',
+    ylabel='Number of ego vehicles',
+    title='Histogram of future valid timestamps for ego vehicles',
+    output_file='ego_future_time_histogram.png', prefix='validation_dataset_')
+
+    df_filtered_agents = df[df['to_predict']== False]
+    get_ego_histogram(df_filtered_agents, 'past_valid_stamps',
+    xlabel='Number of valid timestamps',
+    ylabel='Number of agent vehicles',
+    title='Histogram of past valid timestamps for agent vehicles',
+    output_file='agent_past_time_histogram.png', prefix='validation_dataset_')
+
+    get_ego_histogram(df_filtered_agents, 'future_valid_stamps',
+    xlabel='Number of valid timestamps',
+    ylabel='Number of agent vehicles',
+    title='Histogram of future valid timestamps for agent vehicles',
+    output_file='agent_future_time_histogram.png', prefix='validation_dataset_')
+
+
+def summarize_by_scenario(df):
+    # Ensure boolean column is actually boolean (in case it's read as string)
+    df['to_predict'] = df['to_predict'].astype(bool)
+
+    # Group by scenario
+    grouped = df.groupby('scenario').agg(
+        to_predict_count=('to_predict', lambda x: x.sum()),     # Count of True values
+        interactions_count=('interactions_count', 'first')      # Assumes same value for the group
+    ).reset_index()
+
+    return grouped
+
+def get_interaction_histograms(df):
+    df_summarized = summarize_by_scenario(df)
+
+    for i in range(1,9):
+        get_interactions_histogram(df_summarized, 'interactions_count',
+            xlabel='Number of interaction Tracks',
+            ylabel='Number of Scenarios',
+            title='Histogram of interaction Count per Scenario',
+            output_file='interaction_histogram.png', prefix='validation_dataset_ego'+str(i)+'_', filter=i)
+    
+
+
+
+
+    
+def main():
+    # Example usage
+    csv_file_path = '../../../data/waymo/processed_scenarios_validation.csv'
+    df = pd.read_csv(csv_file_path)
+    # get_count_histograms(df)
+
+    # get_timestamps_histograms(df)
+
+    get_interaction_histograms(df)
+
+    
+
+
+
+
+
+if __name__ == "__main__":  
+    main()
+
+
+
+
