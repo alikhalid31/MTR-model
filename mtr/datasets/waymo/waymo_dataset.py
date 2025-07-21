de009d07fe8689e4414e33cdbe3d745d52390d13
Ali khalid <alikhalid3110@gmail.com>
2025-07-21 18:43:33 -0400

    minor changes

--- mtr/datasets/waymo/waymo_dataset.py
+++ mtr/datasets/waymo/waymo_dataset.py
@@ -22,8 +22,6 @@ class WaymoDataset(DatasetTemplate):
         self.data_path = self.data_root / self.dataset_cfg.SPLIT_DIR[self.mode]
 
         self.infos = self.get_all_infos(self.data_root / self.dataset_cfg.INFO_FILE[self.mode])
-        self.total_examples = len(self.infos)
-
         self.logger.info(f'Total scenes after filters: {len(self.infos)}')
 
     def get_all_infos(self, info_path):
@@ -36,6 +34,8 @@ class WaymoDataset(DatasetTemplate):
 
         for func_name, val in self.dataset_cfg.INFO_FILTER_DICT.items():
             infos = getattr(self, func_name)(infos, val)
+
+        infos = self.filter_info_by_validity(infos)
 
         return infos
 
@@ -54,117 +54,50 @@ class WaymoDataset(DatasetTemplate):
             if valid_mask.sum() == 0:
                 continue
 
-            assert len(cur_info['tracks_to_predict'].keys()) == 3, f"{cur_info['tracks_to_predict'].keys()}"
+            assert len(cur_info['tracks_to_predict'].keys()) == 5, f"{cur_info['tracks_to_predict'].keys()}"
             cur_info['tracks_to_predict']['track_index'] = list(np.array(cur_info['tracks_to_predict']['track_index'])[valid_mask])
             cur_info['tracks_to_predict']['object_type'] = list(np.array(cur_info['tracks_to_predict']['object_type'])[valid_mask])
             cur_info['tracks_to_predict']['difficulty'] = list(np.array(cur_info['tracks_to_predict']['difficulty'])[valid_mask])
+            cur_info['tracks_to_predict']['valid_20s'] = list(np.array(cur_info['tracks_to_predict']['valid_20s'])[valid_mask])
 
             ret_infos.append(cur_info)
         self.logger.info(f'Total scenes after filter_info_by_object_type: {len(ret_infos)}')
         return ret_infos
 
-    def __len__(self):
-        if self.dataset_cfg.SLIDING_WINDOW.ENABLE:
-            total_windows = self.dataset_cfg.SLIDING_WINDOW.TOTAL_WINDOWS / self.dataset_cfg.SLIDING_WINDOW.SLIDING_WINDOW_STEP
-            return len(self.infos) * int(total_windows)
-        else:
-            return len(self.infos)
 
-    def __getitem__(self, index):
-        if self.dataset_cfg.SLIDING_WINDOW.ENABLE:
-            # sliding window mode
-            base_example_index  = index // self.dataset_cfg.SLIDING_WINDOW.TOTAL_WINDOWS
-            window_index = index % self.dataset_cfg.SLIDING_WINDOW.TOTAL_WINDOWS
+    def filter_info_by_validity(self, infos):
+        ret_infos = []
+        for cur_info in infos:
+            num_interested_agents = cur_info['tracks_to_predict']['track_index'].__len__()
+            if num_interested_agents == 0:
+                continue
 
-            if window_index >= self.dataset_cfg.SLIDING_WINDOW.TOTAL_WINDOWS:
-                raise ValueError(f'window_index {window_index} exceeds the total windows {self.dataset_cfg.SLIDING_WINDOW.TOTAL_WINDOWS}')
+            valid_mask = []
+            for idx, cur_track_index in enumerate(cur_info['tracks_to_predict']['track_index']):
+                valid_mask.append(cur_info['tracks_to_predict']['valid_20s'][idx]==1)
 
-            ret_infos = self.create_scene_level_data_sliding_window(base_example_index, window_index)
-        else:
-            ret_infos = self.create_scene_level_data(index)
+            valid_mask = np.array(valid_mask) > 0
+            if valid_mask.sum() == 0:
+                continue
 
+            assert len(cur_info['tracks_to_predict'].keys()) == 5, f"{cur_info['tracks_to_predict'].keys()}"
+            cur_info['tracks_to_predict']['track_index'] = list(np.array(cur_info['tracks_to_predict']['track_index'])[valid_mask])
+            cur_info['tracks_to_predict']['object_type'] = list(np.array(cur_info['tracks_to_predict']['object_type'])[valid_mask])
+            cur_info['tracks_to_predict']['difficulty'] = list(np.array(cur_info['tracks_to_predict']['difficulty'])[valid_mask])
+            cur_info['tracks_to_predict']['valid_20s'] = list(np.array(cur_info['tracks_to_predict']['valid_20s'])[valid_mask])
+
+            ret_infos.append(cur_info)
+
+        # self.logger.info(f'Total scenes after filter_info_by_object_type: {len(ret_infos)}')
         return ret_infos
 
+    def __len__(self):
+        return len(self.infos)
 
-    def create_scene_level_data_sliding_window(self, index,timestamp):
-        """
-        Args:
-            index (index):
+    def __getitem__(self, index):
+        ret_infos = self.create_scene_level_data(index)
 
-        Returns:
-
-        """
-        info = self.infos[index]
-        scene_id = info['scenario_id']
-        with open(self.data_path / f'sample_{scene_id}.pkl', 'rb') as f:
-            info = pickle.load(f)
-
-        sdc_track_index = info['sdc_track_index']
-        current_time_index = info['current_time_index']
-        timestamps = np.array(info['timestamps_seconds'][:current_time_index + 1], dtype=np.float32)
-
-        track_infos = info['track_infos']
-
-        track_index_to_predict = np.array(info['tracks_to_predict']['track_index'])
-        obj_types = np.array(track_infos['object_type'])
-        obj_ids = np.array(track_infos['object_id'])
-        obj_trajs_full = track_infos['trajs']  # (num_objects, num_timestamp, 10)
-        obj_trajs_past = obj_trajs_full[:, :current_time_index + 1]
-        obj_trajs_future = obj_trajs_full[:, current_time_index + 1:]
-
-        center_objects, track_index_to_predict = self.get_interested_agents(
-            track_index_to_predict=track_index_to_predict,
-            obj_trajs_full=obj_trajs_full,
-            current_time_index=current_time_index,
-            obj_types=obj_types, scene_id=scene_id
-        )
-
-        (obj_trajs_data, obj_trajs_mask, obj_trajs_pos, obj_trajs_last_pos, obj_trajs_future_state, obj_trajs_future_mask, center_gt_trajs,
-            center_gt_trajs_mask, center_gt_final_valid_idx,
-            track_index_to_predict_new, sdc_track_index_new, obj_types, obj_ids) = self.create_agent_data_for_center_objects(
-            center_objects=center_objects, obj_trajs_past=obj_trajs_past, obj_trajs_future=obj_trajs_future,
-            track_index_to_predict=track_index_to_predict, sdc_track_index=sdc_track_index,
-            timestamps=timestamps, obj_types=obj_types, obj_ids=obj_ids
-        )
-
-        ret_dict = {
-            'scenario_id': np.array([scene_id] * len(track_index_to_predict)),
-            'obj_trajs': obj_trajs_data,
-            'obj_trajs_mask': obj_trajs_mask,
-            'track_index_to_predict': track_index_to_predict_new,  # used to select center-features
-            'obj_trajs_pos': obj_trajs_pos,
-            'obj_trajs_last_pos': obj_trajs_last_pos,
-            'obj_types': obj_types,
-            'obj_ids': obj_ids,
-
-            'center_objects_world': center_objects,
-            'center_objects_id': np.array(track_infos['object_id'])[track_index_to_predict],
-            'center_objects_type': np.array(track_infos['object_type'])[track_index_to_predict],
-
-            'obj_trajs_future_state': obj_trajs_future_state,
-            'obj_trajs_future_mask': obj_trajs_future_mask,
-            'center_gt_trajs': center_gt_trajs,
-            'center_gt_trajs_mask': center_gt_trajs_mask,
-            'center_gt_final_valid_idx': center_gt_final_valid_idx,
-            'center_gt_trajs_src': obj_trajs_full[track_index_to_predict]
-        }
-
-        if not self.dataset_cfg.get('WITHOUT_HDMAP', False):
-            if info['map_infos']['all_polylines'].__len__() == 0:
-                info['map_infos']['all_polylines'] = np.zeros((2, 7), dtype=np.float32)
-                print(f'Warning: empty HDMap {scene_id}')
-
-            map_polylines_data, map_polylines_mask, map_polylines_center = self.create_map_data_for_center_objects(
-                center_objects=center_objects, map_infos=info['map_infos'],
-                center_offset=self.dataset_cfg.get('CENTER_OFFSET_OF_MAP', (30.0, 0)),
-            )   # (num_center_objects, num_topk_polylines, num_points_each_polyline, 9), (num_center_objects, num_topk_polylines, num_points_each_polyline)
-
-            ret_dict['map_polylines'] = map_polylines_data
-            ret_dict['map_polylines_mask'] = (map_polylines_mask > 0)
-            ret_dict['map_polylines_center'] = map_polylines_center
-
-        return ret_dict
-
+        return ret_infos
 
     def create_scene_level_data(self, index):
         """
@@ -174,23 +107,26 @@ class WaymoDataset(DatasetTemplate):
         Returns:
 
         """
-        info = self.infos[index]
-        scene_id = info['scenario_id']
+        old_info = self.infos[index]
+        scene_id = old_info['scenario_id']
         with open(self.data_path / f'sample_{scene_id}.pkl', 'rb') as f:
             info = pickle.load(f)
 
         sdc_track_index = info['sdc_track_index']
         current_time_index = info['current_time_index']
-        timestamps = np.array(info['timestamps_seconds'][:current_time_index + 1], dtype=np.float32)
+        # current_time_index = 10
+
+        timestamps = np.array(info['timestamps_seconds'][10:current_time_index + 1], dtype=np.float32)
 
         track_infos = info['track_infos']
 
-        track_index_to_predict = np.array(info['tracks_to_predict']['track_index'])
+        track_index_to_predict = np.array(old_info['tracks_to_predict']['track_index'])
         obj_types = np.array(track_infos['object_type'])
         obj_ids = np.array(track_infos['object_id'])
         obj_trajs_full = track_infos['trajs']  # (num_objects, num_timestamp, 10)
-        obj_trajs_past = obj_trajs_full[:, :current_time_index + 1]
+        obj_trajs_past = obj_trajs_full[:, 10:current_time_index + 1]
         obj_trajs_future = obj_trajs_full[:, current_time_index + 1:]
+        # obj_trajs_future = obj_trajs_full[:, current_time_index + 1:current_time_index+11] # to restrict the ground truth to 1 sec in future
 
         center_objects, track_index_to_predict = self.get_interested_agents(
             track_index_to_predict=track_index_to_predict,
@@ -206,6 +142,8 @@ class WaymoDataset(DatasetTemplate):
             track_index_to_predict=track_index_to_predict, sdc_track_index=sdc_track_index,
             timestamps=timestamps, obj_types=obj_types, obj_ids=obj_ids
         )
+
+        # print(obj_trajs_data.shape, 'object trajs data shape')
 
         ret_dict = {
             'scenario_id': np.array([scene_id] * len(track_index_to_predict)),
@@ -271,6 +209,8 @@ class WaymoDataset(DatasetTemplate):
         obj_trajs_future_mask = obj_trajs_future_mask[:, valid_past_mask]  # (num_center_objects, num_objects, num_timestamps_future):
         obj_types = obj_types[valid_past_mask]
         obj_ids = obj_ids[valid_past_mask]
+
+        # print(obj_trajs_data.shape, 'object trajs data shape after filtering')
 
         valid_index_cnt = valid_past_mask.cumsum(axis=0)
         track_index_to_predict_new = valid_index_cnt[track_index_to_predict] - 1
@@ -380,6 +320,7 @@ class WaymoDataset(DatasetTemplate):
             center_heading=center_objects[:, 6],
             heading_index=6, rot_vel_index=[7, 8]
         )
+        # print('object trajs past shape', obj_trajs_past.shape)
 
         ## generate the attributes for each object
         object_onehot_mask = torch.zeros((num_center_objects, num_objects, num_timestamps, 5))
@@ -411,8 +352,18 @@ class WaymoDataset(DatasetTemplate):
             acce,
         ), dim=-1)
 
+        # print(obj_trajs[:, :, :, 0:6].shape, 'object trajs shape')
+        # print(object_onehot_mask.shape, 'object onehot mask shape')
+        # print(object_time_embedding.shape, 'object time embedding shape')
+        # print(object_heading_embedding.shape, 'object heading embedding shape')
+        # print(obj_trajs[:, :, :, 7:9].shape, 'object velocity shape')
+        # print(acce.shape, 'object acceleration shape')  
+        # print('ret_obj_trajs shape:', ret_obj_trajs.shape)
+
         ret_obj_valid_mask = obj_trajs[:, :, :, -1]  # (num_center_obejcts, num_objects, num_timestamps)  # TODO: CHECK THIS, 20220322
         ret_obj_trajs[ret_obj_valid_mask == 0] = 0
+
+        # print('ret_obj_trajs shape:', ret_obj_trajs.shape)
 
         ##  generate label for future trajectories
         obj_trajs_future = torch.from_numpy(obj_trajs_future).float()
@@ -609,14 +560,16 @@ class WaymoDataset(DatasetTemplate):
 
         return pred_dict_list
 
-    def evaluation(self, pred_dicts, output_path=None, eval_method='waymo', **kwargs):
+    def evaluation(self, pred_dicts, output_path=None, eval_method='waymo', eval_sec= 8, **kwargs):
         if eval_method == 'waymo':
             from .waymo_eval import waymo_evaluation
             try:
                 num_modes_for_eval = pred_dicts[0][0]['pred_trajs'].shape[0]
             except:
                 num_modes_for_eval = 6
-            metric_results, result_format_str = waymo_evaluation(pred_dicts=pred_dicts, num_modes_for_eval=num_modes_for_eval)
+                
+
+            metric_results, result_format_str = waymo_evaluation(pred_dicts=pred_dicts, eval_second=eval_sec, num_modes_for_eval=num_modes_for_eval)
 
             metric_result_str = '\n'
             for key in metric_results:
@@ -624,10 +577,35 @@ class WaymoDataset(DatasetTemplate):
                 metric_result_str += '%s: %.4f \n' % (key, metric_results[key])
             metric_result_str += '\n'
             metric_result_str += result_format_str
+
         else:
             raise NotImplementedError
 
         return metric_result_str, metric_results
+
+    def evaluation_custom(self, pred_dicts, output_path=None, eval_method='waymo', eval_sec= 8, **kwargs):
+        if eval_method == 'waymo':
+            from .waymo_eval import waymo_evaluation_custom
+            try:
+                num_modes_for_eval = pred_dicts[0][0]['pred_trajs'].shape[0]
+            except:
+                num_modes_for_eval = 6
+                
+
+            mAP, minADE, minFDE, missRate = waymo_evaluation_custom(pred_dicts=pred_dicts, eval_second=eval_sec, num_modes_for_eval=num_modes_for_eval)
+
+            # metric_result_str = '\n'
+            # for key in metric_results:
+            #     metric_results[key] = metric_results[key]
+            #     metric_result_str += '%s: %.4f \n' % (key, metric_results[key])
+            # metric_result_str += '\n'
+            # metric_result_str += result_format_str
+
+        else:
+            raise NotImplementedError
+
+        # return metric_result_str, metric_results
+        return mAP, minADE, minFDE, missRate
 
 
 if __name__ == '__main__':
